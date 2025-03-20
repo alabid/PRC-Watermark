7015d6a3b33a41335acffd48ca8403a1077e9e7d
Daniel Alabi <alabidan@gmail.com>
2025-03-20 11:58:04 -0400

    try to use cpu for decoding

--- inversion.py
+++ inversion.py
@@ -126,7 +126,7 @@ def exact_inversion(
 
     # prompt to text embeddings
     text_embeddings_tuple = pipe.encode_prompt(
-        prompt, 'cuda', 1, guidance_scale > 1.0, None
+        prompt, device, 1, guidance_scale > 1.0, None
     )
     text_embeddings = torch.cat([text_embeddings_tuple[1], text_embeddings_tuple[0]])
 
